72b560bb172955d79cb1fe736c92e34c41d11ae2
IT24103403 <it24103403@my.sliit.lk>
2024-11-17 09:53:09 +0530

    IT24103403Lab4Q1

---
+++ Q1/IT24103403Lab4Q1.java
@@ -0,0 +1,38 @@
+// Import the Scanner class for taking user input
+import java.util.Scanner;
+
+// Declare the class with the filename as the class name
+public class IT24103403Lab4Q1 {
+
+    // Main method
+    public static void main(String[] args) {
+
+        // Declare an integer variable to store the input number
+        int number;
+
+        // Create a Scanner object
+        Scanner num = new Scanner(System.in);
+
+        // Diaplay to enter number
+        System.out.println("Enter number: ");
+
+        // Read the input number from user
+        number = num.nextInt();
+
+        // if the number is positive
+        if (0 < number) {
+            System.out.println("The number is: Positive"); 
+        }
+        //if the number is negative
+        else if (number < 0) {
+            System.out.println("The number is: Negative"); 
+        }
+        // If the number is neither positive nor negative, it must be zero
+        else {
+            System.out.println("The number is: The number is Zero"); 
+        }
+
+        // Close the Scanner
+        num.close();
+    }
+}
